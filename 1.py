412cd2c9e71ed0ff418a9ee6ea40b92415aca874
Maks <maxtverdohlebov2006@gmail.com>
2021-12-13 18:58:40 +0300

    lesson clas

---
+++ 1.py
@@ -0,0 +1,6 @@
+import math
+r=float(input('Введите радиус'))
+a=2*r*math.sqrt(3)
+s=(a**2*math.sqrt(3))/4
+print("сторона -", a )
+print("площа-", s)
---
+++ else if 1.py
@@ -0,0 +1,54 @@
+#a = 5
+#b = 8
+#c = 7 
+#print("Обьекты ", a, b, c, sep=" ***** ")
+
+#a = 5
+#b = 8
+#c = 7 
+#print( a, b, c, sep=" ***** ", end=":")
+
+#message = input(" Привет, меня зовут bot, как тебя зовут ?")
+#print("Hello, ", message) 
+
+#name = input("Please enter your name: ")
+#a = f"Игра началась, рад приветствовать тебя - {name}"
+#print(a)
+
+#f= 7
+
+#f+=3
+#print(f)
+#f-=4
+#print(f)
+#f*=3
+#print(f)
+#f/=10
+#print(f)
+
+#r = "Den"
+#r += "is"
+#print(r)
+
+#t = " Нажмите пожалуйста enter, для того, чтобы продолжить." 
+#t += "\nКак Вас зовут ?" 
+#name = input(t) 
+#print(f"\nПривет, {name}!")
+
+#q = 20**2
+#print(q)
+
+#p= pow(10, 2)
+#print(p)
+
+
+#t = str(input("Придумай число ? "))
+
+#t = str(input("Придумай число ? "))
+#r = str(input("Придумай число ? "))
+#print("Cool", t+r)
+
+a=4
+b=5
+c=6
+print(list('abd'))
